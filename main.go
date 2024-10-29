5bd63fdeb6959833d73bd56d102f8ecbacf5e13e
Davis <davislimzh1@gmail.com>
2024-10-30 02:42:27 +1100

    tested in main.go using sample.json

--- main.go
+++ main.go
@@ -17,6 +17,27 @@ func main() {
 	orgFolder := folderDriver.GetFoldersByOrgID(orgID)
 
 	folder.PrettyPrint(res)
+	
+	// Print all folders that belongs to orgID
 	fmt.Printf("\n Folders for orgID: %s", orgID)
 	folder.PrettyPrint(orgFolder)
+
+	// Print all children folders of "stunning-horridus"
+	fmt.Printf("\n Get all child folders of %s", orgFolder[4].Name)
+	childFolders, err := folderDriver.GetAllChildFolders(orgID, "stunning-horridus")
+	if err != nil {
+		fmt.Printf("\n Error getting children folders: %v", err)
+		return
+	}
+	fmt.Printf("\n")
+	folder.PrettyPrint(childFolders)
+
+	// Move magnetic-sinister-six and its child under hip-stingray
+	fmt.Printf("\n Move magnetic-sinister-six under hip-stingray")
+	movedFolder, err := folderDriver.MoveFolder("magnetic-sinister-six", "hip-stingray")
+	if err != nil {
+		fmt.Printf("\n Error moving folder: %v", err)
+		return
+	}
+	folder.PrettyPrint(movedFolder)
 }
