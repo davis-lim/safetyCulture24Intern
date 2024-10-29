1be31fcaae53144a26216860b7d231876f953aae
Davis <davislimzh1@gmail.com>
2024-10-30 02:09:32 +1100

    added getMoveSampleFolders function

--- folder/move_folder_test.go
+++ folder/move_folder_test.go
@@ -8,20 +8,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Sample data for testing
-var moveSampleFolders = []folder.Folder{
-	{Name: "alpha", Paths: "alpha", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
-	{Name: "bravo", Paths: "alpha.bravo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
-	{Name: "charlie", Paths: "alpha.bravo.charlie", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
-	{Name: "delta", Paths: "alpha.delta", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
-	{Name: "echo", Paths: "alpha.delta.echo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
-	{Name: "foxtrot", Paths: "foxtrot", OrgId: uuid.Must(uuid.FromString("22222222-2222-2222-2222-222222222222"))},
-	{Name: "golf", Paths: "golf", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+// Returns Sample Folders
+func getMoveSampleFolders() []folder.Folder {
+    return []folder.Folder{
+        {Name: "alpha", Paths: "alpha", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+        {Name: "bravo", Paths: "alpha.bravo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+        {Name: "charlie", Paths: "alpha.bravo.charlie", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+        {Name: "delta", Paths: "alpha.delta", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+        {Name: "echo", Paths: "alpha.delta.echo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+        {Name: "foxtrot", Paths: "alpha.delta.echo.foxtrot", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+        {Name: "golf", Paths: "golf", OrgId: uuid.Must(uuid.FromString("22222222-2222-2222-2222-222222222222"))},
+    }
 }
 
 func Test_folder_MoveFolder(t *testing.T) {
 	// TODO: your tests here
-	driver := folder.NewDriver(sampleFolders)
+	
 
 	tests := []struct {
 		name        string
@@ -36,40 +38,54 @@ func Test_folder_MoveFolder(t *testing.T) {
 			src:  "bravo",
 			dst:  "delta",
 			expected: []folder.Folder{
-				{Name: "alpha", Paths: "alpha", OrgId: moveSampleFolders[0].OrgId},
-				{Name: "bravo", Paths: "alpha.delta.bravo", OrgId: moveSampleFolders[1].OrgId},
-				{Name: "charlie", Paths: "alpha.delta.bravo.charlie", OrgId: moveSampleFolders[2].OrgId},
-				{Name: "delta", Paths: "alpha.delta", OrgId: moveSampleFolders[3].OrgId},
-				{Name: "echo", Paths: "alpha.delta.echo", OrgId: moveSampleFolders[4].OrgId},
-				{Name: "foxtrot", Paths: "foxtrot", OrgId: moveSampleFolders[5].OrgId},
-				{Name: "golf", Paths: "golf", OrgId: moveSampleFolders[6].OrgId},
+				{Name: "alpha", Paths: "alpha", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "bravo", Paths: "alpha.delta.bravo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "charlie", Paths: "alpha.delta.bravo.charlie", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "delta", Paths: "alpha.delta", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "echo", Paths: "alpha.delta.echo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "foxtrot", Paths: "alpha.delta.echo.foxtrot", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "golf", Paths: "golf", OrgId: uuid.Must(uuid.FromString("22222222-2222-2222-2222-222222222222"))},
+			},
+		},
+		{
+			name: "Move delta under bravo",
+			src:  "delta",
+			dst:  "bravo",
+			expected: []folder.Folder{
+				{Name: "alpha", Paths: "alpha", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "bravo", Paths: "alpha.bravo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "charlie", Paths: "alpha.bravo.charlie", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "delta", Paths: "alpha.bravo.delta", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "echo", Paths: "alpha.bravo.delta.echo", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "foxtrot", Paths: "alpha.bravo.delta.echo.foxtrot", OrgId: uuid.Must(uuid.FromString("11111111-1111-1111-1111-111111111111"))},
+				{Name: "golf", Paths: "golf", OrgId: uuid.Must(uuid.FromString("22222222-2222-2222-2222-222222222222"))},
 			},
 		},
 		{
 			name:        "Source folder does not exist",
-			src:         "invalid_folder",
-			dst:         "delta",
+			src:         "testingFolder",
+			dst:         "alpha",
 			hasError: true,
 			expectedErr: "error: source folder does not exist",
 		},
 		{
 			name:        "Destination folder does not exist",
-			src:         "bravo",
-			dst:         "invalid_folder",
+			src:         "echo",
+			dst:         "testingFolder",
 			hasError: true,
 			expectedErr: "error: destination folder does not exist",
 		},
 		{
-			name:        "Move bravo to a folder in a different org (foxtrot)",
-			src:         "bravo",
-			dst:         "foxtrot",
+			name:        "Source and destination belongs in different organisations",
+			src:         "alpha",
+			dst:         "golf",
 			hasError: true,
 			expectedErr: "error: source and destination belongs in different organisations",
 		}, 
 		{
-			name:        "Move bravo to itself",
-			src:         "bravo",
-			dst:         "bravo",
+			name:        "Move folder to itself",
+			src:         "echo",
+			dst:         "echo",
 			hasError: true,
 			expectedErr: "error: source and destination are identical folders",
 		},
@@ -85,9 +101,11 @@ func Test_folder_MoveFolder(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Uses fresh sampleFolders for each test (assumed no persistance)
+			driver := folder.NewDriver(getMoveSampleFolders())
 			result, err := driver.MoveFolder(tt.src, tt.dst)
 
-			if tt.expectedErr != "" {
+			if tt.hasError {
 				// Test for expected errors
 				assert.Error(t, err)
 				assert.EqualError(t, err, tt.expectedErr)
